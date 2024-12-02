bc437be4ea11873ad4a8ee121263ce81103ad8d6
DylanJF04 <dylanfontenot112@gmail.com>
2024-12-01 18:58:33 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+/**
+ *
+ * @author dylan
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize and start the game
+        Game game = new Game();
+        game.startGame();
+    }
+}
